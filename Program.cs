74feb1834b414cede198776c65ac6b640246f2a0
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2026-02-26 05:27:48 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿//******************************************************************************
+//** 1404. Number of Steps to Reduce a Number in Binary Representation to One **
+//******************************************************************************
+//** From right to left the bits we tread,
+//** A quiet carry softly spread.
+//** When ones demand we add, then shift
+//** Till one remains, our final gift.
+//******************************************************************************
+
+int numSteps(char* s)
+{
+    int ans = 0;
+    bool carry = false;
+    int len = strlen(s);
+    int i;
+    char c;
+
+    for (i = len - 1; i > 0; i--)
+    {
+        c = s[i];
+
+        if (carry)
+        {
+            if (c == '0')
+            {
+                c = '1';
+                carry = false;
+            }
+            else
+            {
+                c = '0';
+            }
+        }
+
+        if (c == '1')
+        {
+            ans++;
+            carry = true;
+        }
+
+        ans++;
+    }
+
+    if (carry)
+    {
+        ans++;
+    }
+
+    return ans;
+}
